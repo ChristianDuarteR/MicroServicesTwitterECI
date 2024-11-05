6f8d7aad68db43bf272d8bff8b0fe353c7d4f600
Medina95 <carolina.medina-a@mail.escuelaing.edu.co>
2024-11-05 16:32:15 -0500

    me falto

--- src/main/java/co/edu/escuelaing/microservices/controller/GreetingResource.java
+++ src/main/java/co/edu/escuelaing/microservices/controller/GreetingResource.java
@@ -1,4 +1,4 @@
-//package co.edu.escuelaing.microservices.controller;
+package co.edu.escuelaing.microservices.controller;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -9,31 +9,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-<<<<<<< HEAD
+
 @Path("/")
 public class GreetingResource {
 
- 
-
-    @GET
-    public Response redirigirAIndex() {
-        return Response.status(Response.Status.FOUND)
-                       .header("Location", "/index.html")
-                       .build();
-    }
-    @GET
-@Path("/prueba/")
-public Response prueba(@PathParam("user") String user) {
-    return Response.status(Response.Status.FOUND)
-                   .header("Location", "/front.html" )
-                   .build();
-}
-
-
-
-}
-=======
-//@Path("/")
+ //@Path("/")
 //public class GreetingResource {
 //
 //    @GET
@@ -43,4 +23,18 @@ public Response prueba(@PathParam("user") String user) {
 //                       .build();
 //    }
 //}
->>>>>>> 3c03c88058550cb2599bd13abd288a67422beb83
+
+
+
+@GET
+@Path("/prueba/")
+public Response prueba(@PathParam("user") String user) {
+    return Response.status(Response.Status.FOUND)
+                   .header("Location", "/front.html" )
+                   .build();
+}
+
+
+}
+
+
