f8b8dd269f1f3e6205e4843f51e7ebba9cbb65e8
Medina95 <carolina.medina-a@mail.escuelaing.edu.co>
2024-11-05 14:58:28 -0500

    primer front

--- src/main/java/co/edu/escuelaing/microservices/controller/GreetingResource.java
+++ src/main/java/co/edu/escuelaing/microservices/controller/GreetingResource.java
@@ -1,16 +1,21 @@
 package co.edu.escuelaing.microservices.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public Response redirigirAIndex() {
+        return Response.status(Response.Status.FOUND)
+                       .header("Location", "/index.html")
+                       .build();
     }
 }
